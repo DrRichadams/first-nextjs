155cb7c825ef75f1402fffe145b3db1355f0c92f
DrRichadams <proxyserver7798@gmail.com>
2023-08-31 13:53:31 +0200

    Welcome message

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import classes from "./home.module.css"
 export default function HomePage(props) {
   return (
     <div className={classes.homeBox}>
-      HELLO LUFA
+      <h1>HELLO LUFA</h1>
+      <p>PAGE COMING SOON</p>
     </div>
   )
 }
