bd807a94e480883ee5b6a06754db08ba81d9fb76
DrRichadams <proxyserver7798@gmail.com>
2023-09-22 10:44:32 +0200

    periodic commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import '../styles/globals.css';
-import Layout from '../components/layout/layout';
-import classes from "./home.module.css";
-import WarningModal from '../components/modals/warning-modal';
 
 function MyApp({ Component, pageProps }) {
   return (
       <div>
-        <WarningModal>
-          <h1>HELLO</h1>
-        </WarningModal>
-        <nav className={classes.nav}>
-            <img src="/images/onDark.png" alt="Logo" />
+        <nav>
+            {/* <img src="/images/onDark.png" alt="Logo" /> */}
         </nav>
         <Component {...pageProps} />
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import classes from "./home.module.css"
 
 export default function HomePage(props) {
   return (
-    <div className={classes.homeBox}>
+    <div>
       <h1>HELLO LUFA</h1>
       <p>PAGE COMING SOON</p>
     </div>
