ed3dfa17e6f77f8998e58ff31dd2b7d8b660edd6
DrRichadams <proxyserver7798@gmail.com>
2023-09-29 17:00:14 +0200

    Added navigation
    I have added the navigation bar

--- components/utilities/main_navigation.js
+++ components/utilities/main_navigation.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from "../../styles/Home.module.css";
 
 const MainNavigation = () => {
   return (
-    <nav>
-        I AM NAVIGATION
-    </nav>
+    <div className={styles.nav_main}>
+      <div><img src="/images/logo.png" alt="logo" /></div>
+      <nav>
+          <ul>
+            <li>Projects</li>
+            <li>Socials</li>
+            <li>Contact Us</li>
+          </ul>
+          <div className={styles.cta_btn_box}>
+            <button>Log In</button>
+            <button>Get Started</button>
+          </div>
+      </nav>
+    </div>
   )
 }
 
