2640d10931fabb6f0783313c6fdcd25533b2e344
DrRichadams <proxyserver7798@gmail.com>
2023-10-18 17:00:43 +0200

    Working on the top services
    I have added some color to the top services section

--- components/top_picked_services.js
+++ components/top_picked_services.js
@@ -10,33 +10,35 @@ const TopPicked = () => {
                 <div className={styles.top_service_details_box}>
                     <div className={styles.badge}><p>1</p><h4>Recommended</h4></div>
                     <div className={styles.actual_details}>
-                        <h3>Video Commercials</h3>
+                        <h3 className={styles.top_service_title}>Video Commercials</h3>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quos veritatis quae.ipsum dolor sit amet consectetur adipisicing elit. Vitae quos veritatis quaeipsum dolor sit amet consectetur adipisicing elit. Vitae quos veritatis quae </p>
                         <button>Get Started</button>
                     </div>
                 </div>
-                <div className={styles.top_service_video_box}>VIDEO</div>
+                <div className={styles.top_service_video_box}>
+                    <img src="/images/img3.png" alt="" />
+                </div>
             </div>
             <div className={styles.No2_3}>
                 
                 <div className={styles.No2}>
                     <div className={styles.img_No}>
                         <p>2</p>
-                        <img src="" alt="" />
+                        <img src="/images/img1.png" alt="" />
                     </div>
                     <div className={styles.details_cta}>
-                        <h3>Graphics Designing</h3>
+                        <h3 className={styles.top_service_title}>Graphics Designing</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, quidem commodi! Architecto facere voluptas quisquam amet, doloribus explicabo ullam perspiciatis.</p>
                         <button>Get Started</button>
                     </div>
                 </div>
                 <div className={styles.No3}>
-                <div className={styles.img_No}>
+                    <div className={styles.img_No}>
                         <p>3</p>
-                        <img src="" alt="" />
+                        <img src="/images/img2.png" alt="" />
                     </div>
                     <div className={styles.details_cta}>
-                        <h3>Web Development</h3>
+                        <h3 className={styles.top_service_title}>Web Development</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, quidem commodi! Architecto facere voluptas quisquam amet, doloribus explicabo ullam perspiciatis.</p>
                         <button>Get Started</button>
                     </div>
