cd63e1731d5938c6c9bcce8f326aa2ae1d473988
DrRichadams <proxyserver7798@gmail.com>
2023-12-20 11:27:56 +0200

    Reconstructed protocol
    The top services section has been completely redesigned

--- page_sections/home_page.js
+++ page_sections/home_page.js
@@ -2,13 +2,15 @@ import React from 'react';
 import MainNavigation from '../components/utilities/main_navigation';
 import Banner from '../components/banner_home';
 import TopPicked from '../components/top_picked_services';
+import TopServices from '../components/top_services';
 
 const Home = () => {
   return (
     <div>
         <MainNavigation />
         <Banner />
-        <TopPicked />
+        {/* <TopPicked /> */}
+        <TopServices />
     </div>
   )
 }
